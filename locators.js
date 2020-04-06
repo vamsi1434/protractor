8344834bb2544e032721cb5cb3aed96056bde275
vamsi1434 <35392952+vamsi1434@users.noreply.github.com>
2020-04-06 09:47:57 +0530

    Locators

---
+++ locators.js
@@ -0,0 +1,33 @@
+describe('Protractor test', function() {
+    it('Elements', function() {
+      browser.get('http://juliemr.github.io/protractor-demo/');
+  
+      element(by.model("first")).sendKeys("3");
+      element(by.model("second")).sendKeys("5");
+      element(by.id("gobutton")).click();
+      element(by.model("first")).sendKeys("4");
+      element(by.model("second")).sendKeys("6");
+      element(by.id("gobutton")).click();
+      element(by.model("first")).sendKeys("5");
+      element(by.model("second")).sendKeys("7");
+      element(by.id("gobutton")).click();
+
+      //expect( element(by.css("h2[class='ng-binding']")).getText()).toBe("10");
+      //element(by.css("h2[class='ng-binding']")).getText().then(function(text){
+        //console.log(text);
+     // });
+     //element(by.repeater("result in memory")).element(by.css("td:nth-child(3)")).getText().then(function(text){
+       //console.log(text);
+     //});
+     element.all(by.repeater("result in memory")).each(function(item)
+     {
+       item.element(by.css("td:nth-child(3)")).getText().then(function(text){
+         console.log(text);
+
+     })
+    })
+
+
+      
+    });
+});
